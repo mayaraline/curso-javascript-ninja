84a55055ed83e0744cafb4a870d4bb32a6679672
Mayara Lopes <mayaraline.lopes@gmail.com>
2020-05-05 16:50:46 -0300

    challenges 5-6

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var myarray = ['May', '7', 7, null, undefined];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myfunc(arg){
+    return arg;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(myfunc(myarray)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+var myfunction2 = function(array, arg){
+    return array[arg];
+};
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var array = [null, undefined, 7, true, 'Lopes'];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+myfunction2(array, 0);
+myfunction2(array, 1);
+myfunction2(array, 2);
+myfunction2(array, 3);
+myfunction2(array, 4);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,50 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+var book = function(bookName){
+    var boxhp = {
+        'A Pedra Filosofal':{
+            quantidadePaginas: 223,
+            autor: 'J. K. Rowling',
+            editora: 'Rocco'
+        },
+        'A Camara Secreta':{
+            quantidadePaginas: 252,
+            autor: 'J. K. Rowling',
+            editora: 'Rocco'
+        },
+        'O Prisioneiro de Azkaban':{
+            quantidadePaginas: 318,
+            autor: 'J. K. Rowling',
+            editora: 'Rocco'
+        },
+    };
+
+    return !!boxhp[bookName] ? boxhp[bookName] : boxhp;
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book(''));
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log(`O livro A Pedra Filosofal tem ${book('A Pedra Filosofal').quantidadePaginas} páginas!`);
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log(`O autor do livro A Pedra Filosofal é ${book('A Pedra Filosofal').autor}.`);
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log(`O livro A Pedra Filosofal foi publicado pela editora ${book('A Pedra Filosofal').editora}.`);
