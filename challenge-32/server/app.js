60c8e527a51c430e66a26a4471bda0d2cda65ed2
Mayara Lopes <mayaraline.lopes@gmail.com>
2020-06-01 15:25:05 -0300

    challenge-32

--- challenge-32/server/app.js
+++ challenge-32/server/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
+app.get('/localhost:80', function(req, res) {
   res.json({ message: 'hi' });
 });
 
